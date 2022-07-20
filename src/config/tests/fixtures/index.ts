69156662848b97c606ba608843d9ba9d22249fb4
meli-tortelli <melissa.tortelli@gmail.com>
2022-07-19 23:52:26 -0300

    add helper function to test TaskEither

--- src/config/tests/fixtures/index.ts
+++ src/config/tests/fixtures/index.ts
@@ -1,6 +1,18 @@
 import * as t from "io-ts";
+import { pipe } from "fp-ts/lib/function";
+import * as TE from "fp-ts/TaskEither";
 import { Email } from "@/core/types/scalar";
 
 export function unsafeEmail(value: string): t.Typeof<typeof Email> {
   return value as any;
 }
+
+type Callback = (a: unknown) => unknown;
+
+type MapAll = (
+  fn: Callback
+) => (data: TE.TaskEither<unknown, unknown>) => TE.TaskEither<unknown, unknown>;
+
+export const mapAll: MapAll = (fn) => (data) => {
+  return pipe(data, TE.map(fn), TE.mapLeft(fn));
+};
